e4cdd9128876ba03f12f0f067aea803e948865e4
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-03-17 20:54:48 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Section from "./components/Section";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
        <Header />
+       <Hero />
+       <Benefits/>
       </div>
       <ButtonGradient />
     </div>
